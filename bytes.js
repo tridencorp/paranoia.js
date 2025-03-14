db192983f68915d0b85a4d07466a0cf4895bb3af
damianu <damian@datagentleman.io>
2025-03-14 12:35:15 +0100

    Encode uint arrays

--- bytes.js
+++ bytes.js
@@ -2,12 +2,37 @@ import { log } from "console";
 
 // Encode data to bytes.
 export function encode(...items) {
+  let bytes = [];
 
-  items.forEach((item, _) => {
-    switch (items[0].constructor.name) {
+  items.forEach((item, i) => {
+    switch (items[i].constructor.name) {
+      case "Uint8Array":
+      case "Uint16Array":
+      case "Uint32Array":
+      case "BigUint64Array":
+
+      case "Int8Array":
+      case "Int16Array":
+      case "Int32Array":
+      case "BigIint64Array":
+
+      case "Float32Array":
+      case "Float64Array":
+        let size = new BigUint64Array([BigInt(item.length)]);
+
+        bytes.push.apply(bytes, new Uint8Array(size.buffer));
+        bytes.push.apply(bytes, new Uint8Array(item.buffer));
+        break;
+
       case "Array":
-        log(items[0].constructor.name == "Array");
+        log(items[i].constructor.name);
+        break;
+
+      default:
+        log(items[i].constructor.name)
         break;
     };
   });
+
+  return new Uint8Array(bytes)
 }
--- bytes.test.js
+++ bytes.test.js
@@ -1,11 +1,21 @@
 import { encode } from './bytes.js';
+import { log } from "console";
 import assert from 'assert';
 
-describe('encode', () => {
-  const data = [new ArrayBuffer(20)];
+describe('#encode', () => {
+  let array = new Uint8Array([1, 2, 3]);
+  let want  = new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3]);
+  it('encodes Uint8Array array', () => { assert.deepEqual(encode(array), want) });
 
-  it('should be able to encode arrays', () => {
-    encode(data)
-    assert.equal(true, true);
-  });
+  array = new Uint16Array([1, 2, 3]);
+  want  = new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 3, 0]);
+  it('encodes Uint16Array array', () => { assert.deepEqual(encode(array), want) });
+
+  array = new Uint32Array([1]);
+  want  = new Uint8Array([1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]);
+  it('encodes Uint32Array array', () => { assert.deepEqual(encode(array), want) });
+
+  array = new BigUint64Array([BigInt(1)]);
+  want  = new Uint8Array([1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]);
+  it('encodes Uint32Array array', () => { assert.deepEqual(encode(array), want) });
 });
