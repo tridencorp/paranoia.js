dd1125137b7cfb98778a85913e65f627d05719bc
damianu <damian@datagentleman.io>
2025-03-25 18:11:06 +0100

    Change param naming

--- buffer.js
+++ buffer.js
@@ -1,14 +1,14 @@
 export class Buffer {
+  // TODO: check types.
   constructor(bytes) {
     this.buffer = bytes
     this.offset = 0
   }
 
-  // Read number of bytes and return them as Uint8Array.
-  // TODO: check types.
-  read(number) {
-    const slice = this.buffer.slice(this.offset, this.offset + number)
-    this.offset += number
+  // Read n bytes and return them as Uint8Array.
+  read(n) {
+    const slice = this.buffer.slice(this.offset, this.offset + n)
+    this.offset += n
 
     return slice
   }  
